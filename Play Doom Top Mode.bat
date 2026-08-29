f78afbd9921e93263102588bf5d688d356350756
Tuin <138518202+tuin-boop@users.noreply.github.com>
2026-08-29 17:29:19 +0200

    Use packaged launcher from development folder

--- Play Doom Top Mode.bat
+++ Play Doom Top Mode.bat
@@ -1,8 +1,14 @@
 @echo off
 setlocal
 pushd "%~dp0"
+if exist "%~dp0DoomTopModeLauncher.exe" (
+    "%~dp0DoomTopModeLauncher.exe" %*
+    set "DOOMTOP_EXIT=%ERRORLEVEL%"
+    goto :finished
+)
 powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0play.ps1" %*
 set "DOOMTOP_EXIT=%ERRORLEVEL%"
+:finished
 popd
 if not "%DOOMTOP_EXIT%"=="0" (
     echo.
